4f02e4291225629296a15e2a4164c7753eb28d60
Mikeisfree <mzaperty@gmail.com>
2025-04-28 23:43:19 +0200

    env update

--- lib/supabase-client.ts
+++ lib/supabase-client.ts
@@ -1,10 +1,17 @@
 import { createClient } from "@supabase/supabase-js"
 
 // Initialize Supabase client
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
+  throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_URL')
+}
+if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+  throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY')
+}
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+)
 
 // Define database types
 export interface User {
@@ -58,4 +65,3 @@ begin
 end;
 $$ language plpgsql;
 */
-
